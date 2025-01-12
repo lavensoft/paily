3fe3e510d926953dde90fef6773ccc289441e395
Nhats 👑 <nhatsdevil@gmail.com>
2025-01-12 16:27:04 +0700

    feat(Deposit Method View): + Init page + Add method tiles + Link action in balance card

---
+++ lib/modules/deposit/views/deposit_method.view.dart
@@ -0,0 +1,105 @@
+import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:hugeicons/hugeicons.dart';
+import 'package:paily/shared/themes/app_padding.theme.dart';
+import 'package:paily/shared/themes/app_radius.theme.dart';
+import 'package:paily/shared/widgets/view_appbar.widget.dart';
+
+class DepositMethodView extends HookConsumerWidget {
+  const DepositMethodView({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final theme = Theme.of(context);
+
+    return ColoredBox(
+      color: Colors.white,
+      child: SafeArea(
+        child: Scaffold(
+          appBar: ViewAppBar(
+            title: 'Deposit',
+          ),
+          body: Padding(
+            padding: AppPaddingTheme.viewPadding,
+            child: ListView(
+              children: [
+                ListTile(
+                  leading: Icon(
+                    HugeIcons.strokeRoundedBank,
+                    color: theme.primaryColor,
+                  ),
+                  title: Text(
+                    'Transfer from bank',
+                    style: theme.textTheme.bodyMedium,
+                  ),
+                  subtitle: Text(
+                    'Add money by bank transfer',
+                    style: theme.textTheme.bodySmall,
+                  ),
+                  onTap: () {
+                    // Handle tile tap
+                  },
+                  shape: RoundedRectangleBorder(
+                    borderRadius: AppRadiusTheme.childRadius,
+                  ),
+                ),
+                ListTile(
+                  leading: Icon(
+                    HugeIcons.strokeRoundedBitcoin04,
+                    color: theme.primaryColor,
+                  ),
+                  title: Text(
+                    'Transfer from crypto',
+                    style: theme.textTheme.bodyMedium,
+                  ),
+                  subtitle: Text(
+                    'Add money by USDC or USDT',
+                    style: theme.textTheme.bodySmall,
+                  ),
+                  onTap: () {
+                    // Handle tile tap
+                  },
+                  shape: RoundedRectangleBorder(
+                    borderRadius: AppRadiusTheme.childRadius,
+                  ),
+                ),
+                ListTile(
+                  leading: Icon(
+                    HugeIcons.strokeRoundedLocation01,
+                    color: theme.primaryColor,
+                  ),
+                  title: Text(
+                    'Deposit cash',
+                    style: theme.textTheme.bodyMedium,
+                  ),
+                  subtitle: Text(
+                    'Find the nearest loading location',
+                    style: theme.textTheme.bodySmall,
+                  ),
+                  onTap: () {
+                    // Handle tile tap
+                  },
+                  shape: RoundedRectangleBorder(
+                    borderRadius: AppRadiusTheme.childRadius,
+                  ),
+                )
+              ],
+            ),
+          )
+          
+          // SingleChildScrollView(
+          //   child: Column(
+          //     children: [
+                
+          //       SectionGroup(
+          //         title: 'Transaction Details',
+          //         child: Text('A'),
+          //       )
+          //     ],
+          //   ),
+          // ),
+        ),
+      ),
+    );
+  }
+}
--- lib/modules/home/widgets/balance_card.widget.dart
+++ lib/modules/home/widgets/balance_card.widget.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:hugeicons/hugeicons.dart';
+import 'package:paily/modules/deposit/views/deposit_method.view.dart';
 import 'package:paily/modules/payment/views/payment_account_input.view.dart';
-import 'package:paily/modules/payment/views/payment_amount_input.view.dart';
 import 'package:paily/modules/payment/views/payment_confirm.view.dart';
 import 'package:paily/modules/payment/views/qr_scan.view.dart';
 import 'package:paily/shared/helpers/formatter.helper.dart';
@@ -90,7 +90,7 @@ class BalanceCard extends StatelessWidget {
                 onPressed: () {
                   Navigator.push(
                     context, 
-                    CupertinoPageRoute(builder: (context) => PaymentAmountInputView())
+                    CupertinoPageRoute(builder: (context) => DepositMethodView())
                   );
                 },
               ),
