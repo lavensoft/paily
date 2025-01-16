f5cf7bf468d01c4dd24f3228557b096bd9ee7913
Nhats 👑 <nhatsdevil@gmail.com>
2025-01-17 01:02:02 +0700

    feat(Auth): + Add signin anonymous

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:paily/modules/home/views/home.view.dart';
@@ -16,6 +17,12 @@ void main() async {
     androidProvider: AndroidProvider.debug,
     appleProvider: AppleProvider.debug,
   );
+
+  //!FIXME: For MVP
+  var user = FirebaseAuth.instance.currentUser;
+  if (user == null) {
+    await FirebaseAuth.instance.signInAnonymously();
+  }
 
   runApp(ProviderScope(child: const App()));
 }
