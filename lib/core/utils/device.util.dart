c0a2b60e5e00f0583033f7b6d4a9b007c905b258
Nhats 👑 <nhatsdevil@gmail.com>
2025-01-16 22:03:11 +0700

    feat(DeviceUtil): + getId function
    feat(pubspec):
    + cloud_firestore
    + device_info_plus
    + android_id

---
+++ lib/core/utils/device.util.dart
@@ -0,0 +1,16 @@
+import 'dart:io';
+
+import 'package:android_id/android_id.dart';
+import 'package:device_info_plus/device_info_plus.dart';
+
+class DeviceUtils {
+  static getId() async {
+    var deviceInfo = DeviceInfoPlugin();
+    if (Platform.isIOS) {
+      var iosDeviceInfo = await deviceInfo.iosInfo;
+      return iosDeviceInfo.identifierForVendor;
+    } else if(Platform.isAndroid) {
+      return AndroidId().getId(); 
+    }
+  }
+}
