b93ac6ef839e16d88aed81cb3c41634dc9f48a28
Nhats 👑 <nhatsdevil@gmail.com>
2025-01-18 01:05:08 +0700

    feat(FaceIdAuth): + Implement main faceid auth

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
+import 'package:blur/blur.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:local_auth/local_auth.dart';
 import 'package:paily/modules/home/views/home.view.dart';
 import 'package:paily/shared/themes/app_theme.theme.dart';
 import 'firebase_options.dart';
@@ -39,15 +43,62 @@ void main() async {
   runApp(ProviderScope(child: const App()));
 }
 
-class App extends StatelessWidget {
+class App extends HookWidget {
   const App({super.key});
 
   @override
   Widget build(BuildContext context) {
+    //!HARDCODE For MVP
+    final isAuthenticated = useState(false);
+    final auth = useMemoized(() => LocalAuthentication());
+
+    useEffect(() {
+      authenticate() async {
+        try {
+          final bool didAuthenticate = await auth.authenticate(
+            localizedReason: 'Please authenticate to confirm payment',
+          );
+
+          if (didAuthenticate) {
+            if (context.mounted) {
+              isAuthenticated.value = true;
+            }
+          }
+        } on PlatformException {
+          if (context.mounted) {
+            ScaffoldMessenger
+              .of(context)
+              .showSnackBar(
+                SnackBar(
+                  content: Text(
+                    'Failed to authenticate. Please try again.',
+                  ),
+                ),
+              );
+          }
+        }
+      }
+
+      authenticate();
+      return null;
+    }, []);
+
     return MaterialApp(
       title: 'Paily App',
       theme: AppTheme.light,
-      home: const HomeView(),
+      home: AnimatedSwitcher(
+        duration: const Duration(milliseconds: 900),
+        transitionBuilder: (Widget child, Animation<double> animation) {
+          return FadeTransition(opacity: animation, child: child);
+        },
+        child: isAuthenticated.value
+        ? const HomeView()
+        : Blur(
+            blur: 6,
+            colorOpacity: 0.1,
+            child: const HomeView(),
+          ),
+      ),
     );
   }
 }
