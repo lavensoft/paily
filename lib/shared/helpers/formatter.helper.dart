35944d0e7ae09ba2be9e673299741b77bd57aee9
Nhats 👑 <nhatsdevil@gmail.com>
2025-01-17 13:37:07 +0700

    feat(FormatHelper): + Implement format for VN

--- lib/shared/helpers/formatter.helper.dart
+++ lib/shared/helpers/formatter.helper.dart
@@ -1,8 +1,12 @@
 import 'package:intl/intl.dart';
 
 class FormatHelper {
-  static String formatNumber(double value) {
-    var f = NumberFormat('#,##0.00', 'en_US');
+  static String formatNumber([double? value, String? locale]) {
+    //!HARDCODE
+    var f = NumberFormat(
+      locale == 'vi_VN' ? '#,##0' : '#,##0.00', 
+      locale ?? 'en_US'
+    );
     return f.format(value);
   }
 }
