ccdbab4ee71074d58a25a2bf2b9eb43f8b44f36a
Nhats 👑 <nhatsdevil@gmail.com>
2025-01-17 23:34:04 +0700

    chore(SectionGroup): ~ Fix padding

--- lib/shared/widgets/section_group.widget.dart
+++ lib/shared/widgets/section_group.widget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:paily/shared/themes/app_padding.theme.dart';
 
 class SectionGroup extends StatelessWidget {
   const SectionGroup({
@@ -35,15 +36,22 @@ class SectionGroup extends StatelessWidget {
         ),
         child ?? 
         Flexible(
-          child: OverflowBox(
-            alignment: Alignment.topLeft,
-            maxWidth: MediaQuery.of(context).size.width,
-            child: ListView.separated(
-              shrinkWrap: true,
-              scrollDirection: Axis.horizontal,
-              itemCount: itemCount,
-              itemBuilder: itemBuilder!,
-              separatorBuilder: (context, index) => SizedBox(width: 12),
+          child: Container(
+            transform: Matrix4.translationValues(-15, 0, 0.0),
+            child: OverflowBox(
+              alignment: Alignment.topLeft,
+              maxWidth: MediaQuery.of(context).size.width,
+              child: ListView.separated(
+                shrinkWrap: true,
+                scrollDirection: Axis.horizontal,
+                itemCount: itemCount,
+                itemBuilder: itemBuilder!,
+                separatorBuilder: (context, index) => SizedBox(width: 12),
+                padding: AppPaddingTheme.contentPadding.copyWith(
+                  top: 0,
+                  bottom: 0,
+                ),
+              )
             ),
           )
         ),
